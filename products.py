baa63866541b97146613f3e03e8c5d836a9ea8f5
Ying0705 <yy147716@gmail.com>
2022-04-30 01:25:04 +0800

    basic test

--- products.py
+++ products.py
@@ -4,9 +4,14 @@ while True :
 	if name == 'q':
 		break
 	price = input('please input products price:')
-	p = []
-	p.append(name)
-	p.append(price)
-        products.append(name)
-        products.append(p)
+        products.append([name,price])
 print(products)
+
+for p in products:
+	print(p[0],'price is',p[1])
+
+import io 
+with io.open('products.csv','w', encoding='utf-8') as f:
+	f.write('products,price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
